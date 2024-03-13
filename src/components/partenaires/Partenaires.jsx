61e7797ba95213905d1b987178c82a08aa12f135
HRegniez <henriregniez124@gmail.com>
2024-03-13 15:17:01 +0100

    update

--- src/components/partenaires/Partenaires.jsx
+++ src/components/partenaires/Partenaires.jsx
@@ -9,15 +9,19 @@ const Partenaires = () => {
         <h2>Partenaires</h2>
         <div className='partenaires_flex'>
           <a href="https://www.instagram.com/labo.fl17" target='_blanc'>
-            <img src="src/assets/images/partenaires/Flip17_logo.webp" alt="Flip 17 logo" />
-            <h3>FL17 Lab</h3>
-            <h4>Réalisation de tapis en laine sur mesure.</h4>
+            <div>
+              <img src="src/assets/images/partenaires/Flip17_logo.webp" alt="Flip 17 logo" />
+              <h3 className='partenaires_title'>FL17 Lab</h3>
+              <h4>Réalisation de tapis en laine sur mesure.</h4>
+            </div>
             <p>Laine acrylique, finition en tissu antidérapant. Motifs personnalisés. En création ou à la commande.</p>
           </a>
           <a href="https://www.instagram.com/gypsytattoomarion" target='_blanc'>
-            <img src="src/assets/images/partenaires/Gypsytattoo_logo.svg" alt="Gypsy tattoo logo" />
-            <h3>Gypsy Tattoo</h3>
-            <h4>Tatoueuse</h4>
+            <div>
+              <img src="src/assets/images/partenaires/Gypsytattoo_logo.svg" alt="Gypsy tattoo logo" />
+              <h3 className='partenaires_title'>Gypsy Tattoo</h3>
+              <h4>Tatoueuse</h4>
+            </div>
             <p>Salon privé Bordeaux Caudéran </p>
           </a>
         </div>
